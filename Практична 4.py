679432baf2b33807748dd652105f35f2a5ed7ed0
PavloSavonuk <savonukpavlo556@gmail.com>
2024-12-10 10:31:38 +0200

    Update Практична 4.py

--- Практична 4.py
+++ Практична 4.py
@@ -1,28 +1,17 @@
-dictionary = {
-    "name": "John",
-    "age": 30,
-    "address": {
-        "street": "Baker Street",
-        "city": "London",
-        "postal_code": "NW1",
-        "country": "UK",
-        "building": "221B"
-    },
-    "married": True
-}
+def remove_duplicates(input_list):
+    return list(dict.fromkeys(input_list))
 
-types_dictionary = {
-    "name": str,
-    "age": int,
-    "address": {
-        "street": str,
-        "city": str,
-        "postal_code": str,
-        "country": str,
-        "building": str
-    },
-    "married": bool
-}
 
-print(dictionary)
-print(types_dictionary)
+def sort_list(unique_list):
+    numbers = sorted([x for x in unique_list if isinstance(x, int)])
+    strings = sorted([x for x in unique_list if isinstance(x, str)])
+    return numbers + strings
+
+
+input_list = [3, 1, 2, 3, 4, 5, 6, 3, 4, 5, 7, 6, 5, 4, 3, 4, 5, 4, 3, 'Привіт', 'Анаконда']
+unique_list = remove_duplicates(input_list)
+print("Список без повторень:", unique_list)
+
+
+sorted_list = sort_list(unique_list)
+print("Відсортований список:", sorted_list)
